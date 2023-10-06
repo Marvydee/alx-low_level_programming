023db3964584de973beca55c901e2308c5e96162
Marvellous Divine <marvydee1@gmail.com>
2023-10-06 03:07:17 -0700

    shortened character to fit betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,13 @@
 #include <unistd.h>
-
 /**
  * main - entry point
- *
- * char - declares a character
- *
  * write - prints a statement
- *
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful"" - Dora Korpar, 2015-10-19\n";
-	
-	write(2, message, 59);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, 59);
 	return (1);
 }

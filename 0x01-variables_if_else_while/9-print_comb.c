7a88ae4653c5a601d16aa2d6c19abe0716148624
Marvellous Divine <marvydee1@gmail.com>
2023-10-06 19:18:04 -0700

    number combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * int - declares variable
+ * putchar - prints statement
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < 10; num++)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

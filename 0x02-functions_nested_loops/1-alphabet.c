c18aa6f2cbb645cd43cef255b57e6dc0b67c77ff
Marvellous Divine <marvydee1@gmail.com>
2023-10-10 18:57:46 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * print_alphabet - prints alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}

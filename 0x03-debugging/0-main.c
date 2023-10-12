958e508a196174c10780481ec5e48d84eba494f9
Marvellous Divine <marvydee1@gmail.com>
2023-10-11 23:33:51 -0700

    fixed to betty style

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,13 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: 0
+* Return: Always 0 (success)
 */
-
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+	i = 98
+	positive_or_negative(i);
+	return (0);
 }

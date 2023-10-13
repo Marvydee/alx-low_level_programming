46abd87f948ae123643645ee481847d475ccae57
Marvellous Divine <marvydee1@gmail.com>
2023-10-12 19:40:46 -0700

    prints num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0-9
- * num - number
- * int - declares variable
- * Return: no return
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
  */
 void print_numbers(void)
 {
-	int num;
+	int ch;
 
-	for (num = 0; num <= 9 num++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		_putchar(num);
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
-

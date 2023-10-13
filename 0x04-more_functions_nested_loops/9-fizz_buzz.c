9b0e955ea7e4cda5584064e6ffd89aaef4a4c4b4
Marvellous Divine <marvydee1@gmail.com>
2023-10-13 07:29:22 -0700

    fizz buzz fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz");
-		else if (n % 5 == 0)
+		else if (n % 3 == 0)
 			printf("Fizz");
 		else if (n % 5)
 			printf("Buzz");

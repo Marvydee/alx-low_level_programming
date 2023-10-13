9dcc6c338e4dcd2180ba23068110e299a5025152
Marvellous Divine <marvydee1@gmail.com>
2023-10-12 20:00:19 -0700

    more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers ten times
+ * int - declares variable
+ * _putchar - prints statement
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num < 10; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar((num / 10) + 48);
+				_putchar((num % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}

36f9c1881b341924bd155a4c4663d573aa427d5c
Marvellous Divine <marvydee1@gmail.com>
2023-10-13 07:51:19 -0700

    FIZZBUZZ

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * int - declares a variable
- * Return: Always 0
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
  */
 int main(void)
 {
 	int n;
 
-	for (n = 1;  n <= 100; n++)
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{

32a51cb13ed497e88d166d69fb0c9a2758142fa0
Marvellous Divine <marvydee1@gmail.com>
2023-10-16 17:24:16 -0700

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: Character
+ * Return: no return
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (int start = 0, end = length - 1; start < end; start++, end--)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+	}
+}

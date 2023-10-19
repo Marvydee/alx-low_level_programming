0cbf12f2468e4ec25e49c859b2ac089b50b607d1
Marvellous Divine <marvydee1@gmail.com>
2023-10-19 09:43:38 -0700

    prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
- * Return: no return
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 void print_number(int n)
 {
+	unsigned int m, d, count;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		m = n * -1;
 	}
-	if (n / 10 != 0)
+	else
 	{
-		print_number(n / 10);
+		m = n;
 	}
-	_putchar(n % 10 + '0');
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }

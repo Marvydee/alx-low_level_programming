95a3ea049d53c8ac5fe8a9d6ea187b7a58277915
Marvellous Divine <marvydee1@gmail.com>
2023-10-23 08:01:54 -0700

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @n: number of bytes
+ * @src: source
+ * @dest: destination
+ * Return: return dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (unsigned int i = 0; i < 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}

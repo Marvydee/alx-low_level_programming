57adc0b78d3979c78e3c5dd714f6374afa3b9dc4
Marvydee <marvydee1@gmail.com>
2023-10-24 17:58:48 +0100

    locate a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
+	for (; *s != '\0'; s++)
 		if (*s == c)
 		{
 			return (s);
 		}
-		s++;
-	}
-	return ('\0');
+		else
+		{
+			return ('\0');
+		}
 }

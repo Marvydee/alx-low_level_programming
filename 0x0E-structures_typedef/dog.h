287c908f5129cdb30ac89465476ec8ebafd29c2f
Marvydee <marvydee1@gmail.com>
2023-11-07 13:51:17 +0100

    took out the TYPEDEF from above the struct dog function and placed it below the struct dog function

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,6 @@
 #define _DOG_H_
 
 /**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-
-
-/**
  *struct dog - struct that stores some information of a dog
  *@name: name of the dog
  *@age: age of the dog
@@ -25,6 +17,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
